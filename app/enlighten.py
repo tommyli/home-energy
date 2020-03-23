c65a98e7c1724194c2649ebd171ac97fe5f2ac6d
Tommy Li <tommy.li@firefire.co>
2020-03-23 22:25:22 +1100

    fix: Fixed bug where enlighten data was not fetching because of bad URL

--- app/enlighten.py
+++ app/enlighten.py
@@ -4,7 +4,7 @@ from . import ENLIGHTEN_URL
 
 
 def get_enlighten_stats_resp(api_key, user_id, system_id, as_of_date):
-    path = f"/api/v2/systems{system_id}/stats"
+    path = f"/api/v2/systems/{system_id}/stats"
     query = {'key': api_key, 'user_id': user_id,
              'datetime_format': 'iso8601', 'start_at': f"{int(as_of_date.timestamp())}"}
     resp = requests.get(f"{ENLIGHTEN_URL}{path}", params=query)
